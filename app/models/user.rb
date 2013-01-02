9a20835ed121b9e33eb98c523b36a7b4e37430c0
David Peterson <David.M.Peterson@Cerner.com>
2013-01-02 13:48:14 -0600

    Finish Chapter 08 (signing in)

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ActiveRecord::Base
 
 
   before_save { self.email.downcase! }
+  before_save :create_remember_token
 
   validates :name,  presence: true, length: { maximum: 50 }
 
@@ -27,6 +28,10 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
 
+  private
 
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 
 end
